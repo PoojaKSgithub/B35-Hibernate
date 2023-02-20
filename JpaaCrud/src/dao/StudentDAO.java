28ba6d7f2264d5ed568e783b77ad5c1a7fe253b9
DELL <DELL@DESKTOP-JJ4UU0I>
2023-02-20 22:12:00 +0530

    first commit

---
+++ JpaaCrud/src/dao/StudentDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+import org.tnsindia.entities.Student;
+
+public interface StudentDAO {
+	
+	//abstract method for CRUD 
+	void addStudent(Student student);
+	void updateStudent(Student student);
+	void deleteStudent(Student student);
+	Student getStudentByID(int RollNo);
+
+	void beginTransaction();
+	void commitTransaction();
+
+}
---
+++ JpaaCrud/src/services/StudentService.java
@@ -0,0 +1,15 @@
+package services;
+
+import org.tnsindia.entities.Student;
+
+public interface StudentService {
+	
+	//abstract method for CRUD 
+		void create(Student student);
+		void update(Student student);
+		void delete(Student student);
+		Student retrieve(int RollNo);
+
+		
+
+}
